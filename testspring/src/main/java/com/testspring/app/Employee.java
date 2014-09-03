f9bab76031c3c70ad08d0e51a909bf20ded234d8
Piotr Rosłaniec <p.roslaniec@gmail.com>
2014-09-03 13:25:05 +0200

    Added optional annotations in Employee.java

--- testspring/src/main/java/com/testspring/app/Employee.java
+++ testspring/src/main/java/com/testspring/app/Employee.java
@@ -58,3 +58,69 @@ public class Employee {
         this.id = id;
     }
 }
+/**
+ * Alternative mapping using annotations.
+ * In this case using Employee.hbm.xml is redundant
+ * Also, delete :
+ *      <!-- List of XML mapping files -->
+ *      <mapping resource="Employee.hbm.xml"/>
+ *  from hibernate.cfg.xml
+
+ @Entity
+public class Employee {
+
+    @Id @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "salary")
+    private int salary;
+
+    public Employee() {}
+
+    public Employee(String fname, String lname, int salary) {
+        firstName = fname;
+        lastName = lname;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
+*/
